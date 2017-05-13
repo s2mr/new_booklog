8af2a71c718ca816c93fbebaf78f00d166b3c120
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-05-14 01:13:34 +0900

    マイグレーションの変更とか

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   scope :find_newest_article, -> { order(created_at: :desc).limit(5) }
+
+  validates :title, length: { maxixmum: 50 }, presense: true
+  validates :body, length: { maxixmum: 1000 }, presense: true
+  validates :category, length: { maxixmum: 50 }, presense: true
 end
--- db/migrate/20170512142602_create_posts.rb
+++ db/migrate/20170512142602_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :title, length: { maxixmum: 50 }, presense: true
-      t.text :body, length: { maxixmum: 1000 }, presense: true
-      t.string :category, length: { maxixmum: 50 }, presense: true
+      t.string :title
+      t.text :body
+      t.string :category
 
       t.timestamps
     end
