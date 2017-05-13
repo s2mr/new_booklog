054d2c7eab049a751fab26d697286d02dc2bc5f4
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-05-14 01:35:12 +0900

    新規投稿画面の作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   scope :find_newest_article, -> { order(created_at: :desc).limit(5) }
 
-  validates :title, length: { maxixmum: 50 }, presense: true
-  validates :body, length: { maxixmum: 1000 }, presense: true
-  validates :category, length: { maxixmum: 50 }, presense: true
+  validates :title, length: { maximum: 50 }, presence: true
+  validates :body, length: { maximum: 1000 }, presence: true
+  validates :category, length: { maximum: 50 }, presence: true
 end
