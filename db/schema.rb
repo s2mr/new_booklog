ef798512ae62e5b3c7edc90925f1412a050e8950
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-05-16 20:47:52 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512151805) do
+ActiveRecord::Schema.define(version: 20170516113734) do
 
   create_table "authors", force: :cascade do |t|
     t.string "email", default: "", null: false
